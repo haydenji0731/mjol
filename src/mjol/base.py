863db7d59db6f5dcbfafc1d1f2027fb2f3250e5f
haydenji0731 <hji20@jhu.edu>
2025-07-08 09:27:40 -0400

    WIP: add cds logic & itree implementation

--- src/mjol/base.py
+++ src/mjol/base.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional, Dict, List, ForwardRef
+from intervaltree import IntervalTree
 
 GFeatureRef = ForwardRef("GFeature")
 
@@ -13,12 +14,14 @@ class GFeature(BaseModel):
     strand : str # ['.', '-', '+']
     frame : str # ['.', '0', '1', '2']
     children : List[GFeatureRef] # type: ignore
+    divider : Optional[int] = None
     attributes : Dict[str, str]
     parent : Optional[str] = None
     id : Optional[str] = None
 
     def __init__(self, **data):
         super().__init__(**data)
+        # TODO: fix this
         self.id = self._infer('id')
         self.parent = self._infer('parent')
     
@@ -27,12 +30,14 @@ class GFeature(BaseModel):
             if kwd in k.lower():
                 return v
         return None
-
-    def build_itree_from_children(self):
-        raise NotImplementedError
     
     def __repr__(self) -> str:
         return f"{self.feature_type}:{self.id},{self.strand},{self.start}-{self.end}"
+
+    def add_a_child(self, child, divide : bool = True):
+        if divide:
+            self.divider = len(self.children)
+        self.children.append(child)
 
     def to_gff_entry(self) -> str:
         attributes_str =";".join(f"{key}={value}" for key, value in self.attributes.items())
@@ -42,7 +47,29 @@ class GFeature(BaseModel):
             ]
         ])
         return gff_entry + '\n'
+
+class IntervalGFeature(GFeature):
+    itree: IntervalTree = Field(default_factory=IntervalTree)
+    tie_breaking_pad : float = 0.1
+
+    model_config = {
+        "arbitrary_types_allowed": True
+    }
+
+    def add_a_child(self, child : GFeature):
+        self.children.append(child)
+        if child.start == child.end:
+            self.itree[child.start : child.end + self.tie_breaking_pad] = child
+        else:
+            self.itree[child.start : child.end] = child
     
+    def query_itree(self, start, end):
+        if start == end:
+            res = self.itree[start : end + self.tie_breaking_pad]
+        else:
+            res = self.itree[start : end]
+        return res
+
 def load_attributes(s: str, kv_sep: str = '=') -> dict:
     return {
         k.strip(): v.strip()
